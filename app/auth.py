6a7861b754d3e3d7a9a04e9534963d8b91786c47
benjamin muchemi <benjamin.muchemi@student.moringaschool.com>
2021-08-21 18:25:53 +0300

    Add error messages when user signs up with invalid credentials

--- app/auth.py
+++ app/auth.py
@@ -1,17 +1,38 @@
-from flask import Blueprint,render_template
+from flask import Blueprint,render_template,request,flash
 
 auth = Blueprint('auth',__name__)
 
-@auth.route('/login')
+@auth.route('/login',methods=['GET','POST'])
 def login():
+    data = request.form
+    print(data)
     return render_template('login.html',boolean = True)  
 
 @auth.route('/logout')
 def logout():
     return
     
-@auth.route('/sign-up')
+@auth.route('/sign-up',methods=['GET','POST'])
 def signup():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        first_name = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+
+        if len(email) < 4:
+            flash('Email must be greater than 4 characters',category='error')
+        elif  len(first_name) < 3:
+            flash('Firstname must be greater than 3 characters',category='error')
+        elif  password1 != password2:
+            flash('Passwords do not match',category='error')
+        elif  len(password1) < 6:
+            flash('Passwords must be greater than 6 charachters',category='error')
+        else :
+            flash('Account created successfully',category='success')
+            
+
     return render_template('sign-up.html')
     
 
