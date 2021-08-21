04876f563d640b21d5c6ae90c2b25cf9a770c88f
benjamin muchemi <benjamin.muchemi@student.moringaschool.com>
2021-08-21 17:37:22 +0300

    Create pages for login and signup

--- app/views.py
+++ app/views.py
@@ -1,8 +1,10 @@
-from flask import Blueprint
+from flask import Blueprint,render_template
 
 views = Blueprint('views',__name__)
 
 
 @views.route('/')
 def home():
-    return '<hi>Helloo</h2>'
+    return render_template('home.html')
+
+
